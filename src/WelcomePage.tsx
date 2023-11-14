1c7682b8d7dd6c1c762c5549149ed8f4d8bf283f
Anastasia2403 <nastyajuzufichn@gmail.com>
2023-11-14 22:54:07 +0200

    commit

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -1,7 +1,19 @@
 import Cookies from "js-cookie";
+import jwt from "jsonwebtoken";
+import { useEffect } from "react";
 
 export const WelcomePage = () => {
     const email = Cookies.get('email');
+    const token = Cookies.get('jwt-token');
+
+    useEffect(() => {
+        if(!token) {
+            return;
+        }
+        const decodedToken = jwt.decode(token);
+
+        console.log(decodedToken);
+    }, []);
     return(
       <div>
         <h1>Welcome</h1>
